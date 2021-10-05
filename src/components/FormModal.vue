b23f2107a936605db6fa8fb9f2b6a7ef08fc3384
TsarYurii <tsar.yurii.wm@gmail.com>
2021-10-05 17:49:20 +0300

    Commit

--- src/components/FormModal.vue
+++ src/components/FormModal.vue
@@ -14,56 +14,64 @@
             <span>Create or edit user</span>
           </header>
           <section class="simple-modal-body">
-            <form @submit.prevent="onSubmitForm">
+            <Form
+              :initial-values="initialValues"
+              :validation-schema="formValidationSchema"
+              @submit="onSubmitForm"
+            >
               <div class="mb-3">
                 <span>Name</span>
-                <input
-                  
+                <Field
+                  name="name"
                   type="text"
                   class="form-control"
-                  :placeholder="[this.fake ? this.fake.name : '']"
-                  v-model="name"
+                  placeholder="Your name"
                 />
+                <ErrorMessage class="btn" name="name" />
               </div>
               <div class="mb-3">
                 <span>Email</span>
-                <input
-                  
+                <Field
+                  name="email"
                   type="email"
                   class="form-control"
-                  :placeholder="[this.fake ? this.fake.email : '']"
+                  placeholder="Your email"
                   v-model="email"
                 />
+                <ErrorMessage name="email" />
               </div>
               <div class="mb-3">
                 <span>Street</span>
-                <input
-                  
+                <Field
+                  name="street"
                   type="text"
                   class="form-control"
-                  :placeholder="[this.fake ? this.fake.street : '']"
+                  placeholder="Your street"
                   v-model="street"
                 />
+                <ErrorMessage name="street" />
               </div>
               <div class="mb-3">
                 <span>City</span>
-                <input
-                  
+                <Field
+                  name="city"
                   type="text"
                   class="form-control"
-                  :placeholder="[this.fake ? this.fake.city : '']"
+                  placeholder="Your city"
                   v-model="city"
                 />
+                <ErrorMessage name="city" />
               </div>
               <div class="mb-3">
                 <span>Zip</span>
-                <input
-                  
+                <Field
+                  name="zip"
                   type="text"
                   class="form-control"
-                  :placeholder="[this.fake ? this.fake.zip : '']"
+                  placeholder="Your zip"
                   v-model="zip"
                 />
+                <ErrorMessage name="zip" />
               </div>
               <input class="btn btn-primary" type="submit" value="Submit" />
               <button
@@ -73,7 +81,7 @@
               >
                 Close
               </button>
-            </form>
+            </Form>
           </section>
         </div>
       </div>
@@ -82,8 +90,14 @@
 </template>
 
 <script>
+import { Field, Form, ErrorMessage } from "vee-validate";
 export default {
   name: "FormModal",
+  components: {
+    Field,
+    Form,
+    ErrorMessage,
+  },
   props: {
     fake: {
       type: Object,
@@ -93,17 +107,55 @@ export default {
     },
   },
   data() {
+    const formValidationSchema = {
+      name(name) {
+        if(name !== ""){
+        return true
+      }
+      return "YOU MUST WRITE YOUR NAME, BOY!!!"
+      },
+      email(value) {
+        let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        if(value !== "" && re.test(value)){
+        return true
+      }
+      return "YOU MUST WRITE YOUR EMAIL, BOY!!!"
+      },
+      street(value) {
+        if(value !== ""){
+        return true
+      }
+      return "YOU MUST WRITE YOUR STREET, BOY!!!"
+      },
+      city(value) {
+        if(value !== ""){
+        return true
+      }
+      return "YOU MUST WRITE YOUR CITY, BOY!!!"
+      },
+      zip(value) {
+        if(value !== ""){
+        return true
+      }
+      return "YOU MUST WRITE YOUR ZIP, BOY!!!"
+      },
+    };
     return {
-      name: "",
-      email: "",
-      street: "",
-      city: "",
-      zip: "",
+      name: this.fake ? this.fake.name : "",
+      email: this.fake ? this.fake.email : "",
+      street: this.fake ? this.fake.street : "",
+      city: this.fake ? this.fake.city : "",
+      zip: this.fake ? this.fake.zip : "",
+      formValidationSchema,
+      initialValues:{
+        name: this.name,
+        email: this.email,
+        street: this.street
+      }
     };
   },
   methods: {
     onSubmitForm() {
-
       this.$emit("onSubmitForm", {
         name: this.name.charAt(0).toUpperCase() + this.name.slice(1),
         email: this.email,
@@ -115,6 +167,36 @@ export default {
     close() {
       this.$emit("close");
     },
+    // nameRules(){
+    //   if(this.name !== ""){
+    //     return true
+    //   }
+    //   return "YOU MUST WRITE YOUR NAME, BOY!!!"
+    // },
+    // emailRules(){
+    //   if(this.email !== ""){
+    //     return true
+    //   }
+    //   return "YOU MUST WRITE YOUR EMAIL!!!"
+    // },
+    // streetRules(){
+    //   if(this.street !== ""){
+    //     return true
+    //   }
+    //   return "YOU MUST WRITE YOUR STREET!!!"
+    // },
+    // cityRules(){
+    //   if(this.city !== ""){
+    //     return true
+    //   }
+    //   return "YOU MUST WRITE YOUR CITY!!!"
+    // },
+    // zipRules(){
+    //   if(this.zip !== ""){
+    //     return true
+    //   }
+    //   return "YOU MUST WRITE YOUR ZIP!!!"
+    // }
   },
 };
 </script>
--- src/components/NewUser.vue
+++ src/components/NewUser.vue
@@ -22,34 +22,27 @@ export default {
   methods: {
     ...mapActions(["showModal", "addNewUser"]),
     onSubmitForm(data) {
-      if(data.name === ""){
-        alert("Укажите ваше имя")
-      } else
-      if (data.email === ""){
-        alert("Укажите ваш эмейл")
-      } else
-      if (data.street === ""){
-        alert("Davay pishi ulitsu")
-      } else
-      if(data.city === ""){
-        alert("City")
-      } else
-      if(data.zip === ""){
-        alert("Zip")
-      } else {
+      // if (data.name === "") {
+      //   alert("Name");
+      // } else if (data.email === "") {
+      //   alert("Email");
+      // } else if (data.street === "") {
+      //   alert("Street");
+      // } else if (data.city === "") {
+      //   alert("City");
+      // } else if (data.zip === "") {
+      //   alert("Zip");
+      // } else { }
         const newUser = {
-        name: data.name,
-        email: data.email,
-        street: data.street,
-        city: data.city,
-        zip: data.zip,  
-        id: this.allFilteredData.length + 1,
-        icon: this.allFilteredData[0].icon,
-      };
-      this.addNewUser(newUser);
-      }
-
-
+          name: data.name,
+          email: data.email,
+          street: data.street,
+          city: data.city,
+          zip: data.zip,
+          id: this.allFilteredData.length + 1,
+          icon: this.allFilteredData[0].icon,
+        };
+        this.addNewUser(newUser);
       
     },
     close() {
